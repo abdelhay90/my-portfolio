241926336b5515aedf08411619775c8f601cd777
Ahmed Abdelhay <ahmed.abdelhay@andela.com>
2019-12-09 19:07:39 +0200

    first flight

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import ReactGA from 'react-ga';
 import './App.css';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import About from './Components/About';
+import Resume from './Components/Resume';
+// import Testimonials from './Components/Testimonials';
+import Portfolio from './Components/Portfolio';
+import { data } from './common/constants';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      foo: 'bar',
+      resumeData: {},
+    };
+
+    // ReactGA.initialize('UA-110570651-1');
+    // ReactGA.pageview(window.location.pathname);
+
+  }
+
+  getResumeData() {
+    this.setState({ resumeData: data });
+  }
+
+  componentDidMount() {
+    this.getResumeData();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header data={this.state.resumeData.main}/>
+        <About data={this.state.resumeData.main}/>
+        <Resume data={this.state.resumeData.resume}/>
+        <Portfolio data={this.state.resumeData.portfolio}/>
+        {/*<Testimonials data={this.state.resumeData.testimonials}/>*/}
+        <Footer data={this.state.resumeData.main}/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/common/constants.js
@@ -0,0 +1,223 @@
+export const profile = {
+  firstName: 'Ahmed',
+  fullname: 'Ahmed Abdelhay',
+  email: 'ahmed.abdelhay.90@gmail.com',
+  title: 'Frontend Engineer',
+  github: 'abdelhay90',
+  linkedIn: 'ahmed-abdelhay-5b3b9620/',
+  facebook: 'abdelhay90',
+  resume: 'https://drive.google.com/file/d/19i2L3_hZ3SxOCPwC_zARP-cJZzT7bmnx/view?usp=sharing',
+};
+
+export const workingProjects = {
+  data_gear: [],
+  esri: [],
+  vodafone: [],
+  virgin_gates: [],
+  andela: [],
+};
+
+export const data = {
+  'main': {
+    'name': 'Ahmed Abdelhay',
+    'occupation': 'Frontend Engineer',
+    'description': 'Here will be your description. Use this to describe what you do or whatever you feel best describes yourself to a potential employer',
+    'image': 'me_profile.jpg',
+    'bio': 'Use this bio section as your way of describing yourself and saying what you do, what technologies you like to use or feel most comfortable with, describing your personality, or whatever else you feel like throwing in.',
+    'contactmessage': 'Here is where you should write your message to readers to have them get in contact with you.',
+    'email': 'ahmed.abdelhay.90@gmail.com',
+    'phone': '+20 100 470 1196',
+    'address': {
+      'city': 'Cairo',
+      'state': 'Egypt',
+    },
+    'website': '/',
+    'resumedownload': 'https://drive.google.com/file/d/19i2L3_hZ3SxOCPwC_zARP-cJZzT7bmnx/view?usp=sharing',
+    'social': [
+      {
+        'name': 'facebook',
+        'url': 'http://facebook.com/abdelhay90',
+        'className': 'fa fa-facebook',
+      },
+      {
+        'name': 'linkedin',
+        'url': 'https://www.linkedin.com/in/ahmed-abdelhay-5b3b9620/',
+        'className': 'fa fa-linkedin',
+      },
+      {
+        'name': 'github',
+        'url': 'http://github.com/abdelhay90',
+        'className': 'fa fa-github',
+      },
+      {
+        'name': 'skype',
+        'url': 'a.abd.elhay',
+        'className': 'fa fa-skype',
+      },
+    ],
+  },
+  'resume': {
+    'skillmessage': 'Here you can create a short write-up of your skills to show off to employers',
+    'education': [
+      {
+        'school': 'Ain Shams University',
+        'degree': 'Bachelor of computer science',
+        'graduated': '2011',
+        'description': 'Studying Computer Science, Algorithms Design and Analysis, Computer Vision, Data Structures, Compiles, etc...',
+      },
+    ],
+    'work': [
+      {
+        'company': 'Andela',
+        'title': 'Senior Software Engineer',
+        'years': 'March 2019 - Present',
+        'description': 'Describe work, special projects, notable achievements, what technologies you have been working with, and anything else that would be useful for an employer to know.',
+      },
+      {
+        'company': 'Virgin Gates',
+        'title': 'Senior / Principal Frontend Engineer',
+        'years': 'Sept. 2018 - Jan. 2019',
+        'description': 'Describe work, special projects, notable achievements, what technologies you have been working with, and anything else that would be useful for an employer to know.',
+      },
+      {
+        'company': 'Vodafone',
+        'title': 'Senior Frontend Engineer',
+        'years': 'Jan. 2018 - Sept. 2018',
+        'description': 'Describe work, special projects, notable achievements, what technologies you have been working with, and anything else that would be useful for an employer to know.',
+      },
+      {
+        'company': 'esri Northeast Africa',
+        'title': 'Senior Frontend Engineer',
+        'years': 'May 2014 - Jan. 2018',
+        'description': 'Describe work, special projects, notable achievements, what technologies you have been working with, and anything else that would be useful for an employer to know.',
+      },
+      {
+        'company': 'Data Gear',
+        'title': '.Net Developer',
+        'years': 'Feb. 2013 - April 2014',
+        'description': 'Describe work, special projects, notable achievements, what technologies you have been working with, and anything else that would be useful for an employer to know.',
+      },
+    ],
+    'skills': [
+      {
+        'name': 'Javascript',
+        'level': '75%',
+      },
+      {
+        'name': 'ReactJs',
+        'level': '65%',
+      },
+      {
+        'name': 'VueJs ',
+        'level': '65%',
+      },
+      {
+        'name': 'Angular ',
+        'level': '65%',
+      },
+      {
+        'name': 'Node.js ',
+        'level': '60%',
+      },
+      {
+        'name': 'CSS',
+        'level': '60%',
+      },
+      {
+        'name': 'HTML5',
+        'level': '60%',
+      },
+      {
+        'name': 'SASS',
+        'level': '50%',
+      },
+      {
+        'name': 'LESS',
+        'level': '50%',
+      },
+      {
+        'name': 'Jquery',
+        'level': '70%',
+      },
+      {
+        'name': 'Dojo',
+        'level': '70%',
+      },
+      {
+        'name': 'esri JS API',
+        'level': '70%',
+      },
+      {
+        'name': 'MapBox',
+        'level': '60%',
+      },
+      {
+        'name': 'Rest',
+        'level': '70%',
+      },
+      {
+        'name': 'Git',
+        'level': '60%',
+      },
+      {
+        'name': 'Docker',
+        'level': '50%',
+      },
+      {
+        'name': 'Kubernetes',
+        'level': '40%',
+      },
+      {
+        'name': 'Google Cloud',
+        'level': '40%',
+      },
+      {
+        'name': 'Travis CI',
+        'level': '50%',
+      },
+      {
+        'name': 'Postgre SQL',
+        'level': '50%',
+      },
+      {
+        'name': 'MongoDB',
+        'level': '50%',
+      },
+
+    ],
+  },
+  'portfolio': {
+    'projects': [
+      {
+        'title': 'Vodafone DE',
+        'category': 'Vodafone Germany WebApp',
+        'image': 'canadian-wanderlust.jpg',
+        'url': 'https://www.vodafone.de',
+      },
+      {
+        'title': 'SURVV',
+        'category': 'SURVV Premium Delivery Service',
+        'image': 'fury-fighting-gear.jpg',
+        'url': 'https://survv.com',
+      },
+      {
+        'title': 'MG Suite',
+        'category': 'Multi-Gen data display and location services',
+        'image': 'original-thai-food.jpg',
+        'url': 'http://www.nextgss.com',
+      }
+    ],
+  },
+  'testimonials': {
+    'testimonials': [
+      {
+        'text': 'Here you should write some nice things that someone has said about you. No lies though, employers can tell when you are lying.',
+        'user': 'Kareem Abdul Jabbar',
+      },
+      {
+        'text': 'That Tim Baker must be one of the most brilliant developers I\'ve ever met! It is amazing that nobody has hired him yet. Hey you, you should hire this guy, he may be fresh out of University and have zero on the job experience but I am confident that he will be one of your best developers in no time!',
+        'user': 'Steve Wozniak... impersonator',
+      },
+    ],
+  },
+};
