595cec8c602ec54be25a851958a3d9f5d520d42d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-10 14:16:36 +0000

    Deploy abdelhay90/my-portfolio to github.com/abdelhay90/my-portfolio.git:gh-pages

---
+++ precache-manifest.15754e331ca34a919b657ae18447f446.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ebaccf38bc227399d90490385c05f02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06e92ed05f9d141cff25",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "b669ce5ada3c49b4a2ca",
+    "url": "/static/js/2.5dd77941.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.5dd77941.chunk.js.LICENSE"
+  },
+  {
+    "revision": "06e92ed05f9d141cff25",
+    "url": "/static/js/main.05e61155.chunk.js"
+  },
+  {
+    "revision": "50f3c10a873c803cc1cf",
+    "url": "/static/js/runtime-main.31e4fea3.js"
+  }
+]);
